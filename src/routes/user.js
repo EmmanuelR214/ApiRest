088d1d60b607d513143b5c1755119e92ba070700
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2023-03-10 07:45:42 -0600

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,11 +4,19 @@ const userSchema = require('../models/user')
 const router = express.Router()
 
 //Crear usuarios
-
 router.post('/users', (req, res) => {
   const user = userSchema(req.body)
   user
     .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+})
+
+//mostrar todos los datos
+
+router.get('/users', (req, res) => {
+  userSchema
+    .find()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 })
