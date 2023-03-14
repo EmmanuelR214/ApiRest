ebf8360b5da8b8a2e85d9d69f709b579c4a8301c
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2023-03-13 20:40:32 -0600

    Add files via upload

---
+++ src/routes/Iot.js
@@ -0,0 +1,33 @@
+const express =  require("express")
+const userSchema = require('../models/Iot')
+
+const router = express.Router()
+
+//Insertar un dato
+router.post('/iot', (req, res) => {
+  const user = userSchema(req.body)
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+})
+
+//mostrar todos los datos
+router.get('/iot', (req, res) => {
+    userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+})
+
+//actualizar un usuario
+router.put('/iot/:id', (req, res) => {
+  const {id} = req.params
+  const {telefono, Estado, Ciudad, colonia, calle, descripcion} = req.body
+  userSchema
+    .updateOne({_id: id}, { $set: {telefono, Estado, Ciudad, colonia, calle, descripcion}})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+})
+
+module.exports = router
