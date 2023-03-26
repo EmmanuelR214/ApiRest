cbbbbf031307b6a1f3f287f98f1f5cd33bd04003
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2023-03-26 04:32:41 -0600

    Update venta.js

--- src/routes/venta.js
+++ src/routes/venta.js
@@ -28,7 +28,7 @@ router.post('/venta', async (req, res) => {
 });
 
 //mostrar todos los datos
-router.get('/venta', (req, res) => {
+router.get('/venta/total', (req, res) => {
     ventas
         .find()
         .then((data) => res.json(data))
