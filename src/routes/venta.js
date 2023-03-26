326e0986c3af53229f7f85dbd502742880701dfd
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2023-03-25 19:12:08 -0600

    Add files via upload

---
+++ src/routes/venta.js
@@ -0,0 +1,58 @@
+const express =  require("express")
+const ventas = require("../models/ventas")
+
+const router = express.Router()
+
+//Insertar un dato
+router.post('/venta', async (req, res) => {
+    const {cantidad, total, usuario, producto} = req.body;
+    
+    try {
+        const fechaAct = Date()
+        
+        const shop = new ventas({
+            fecha: fechaAct,
+            cantidad,
+            total,
+            usuario,
+            producto
+        });
+        
+        await shop.save();
+        
+        res.status(201).json({ message: 'venta registrada con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error Interno del Servidor' });
+    }
+});
+
+//mostrar todos los datos
+router.get('/venta', (req, res) => {
+    ventas
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+})
+
+//encontrar un usuario especifico por id
+router.get('/venta/user/:usuario', (req, res) => {
+    ventas
+        .findOne({usuario: req.params.usuario})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+})
+
+
+module.exports = router
+
+/*
+router.post('/venta', (req, res) => {
+    
+    const user = Ventas(req.body)
+    user
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+})
+*/
